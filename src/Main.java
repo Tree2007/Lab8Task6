479dae90c32ad4ceaaa20e05c7b48ef810bab5f1
Forest <fhaughton6@gmail.com>
2024-11-12 08:15:13 -0500

    123

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        String thing;
+        thing = scan.nextLine();
+        prettyHeader(thing);
     }
+    public static void prettyHeader(String msg) {
+
+        for (int i = 0; i < 60; i++) {
+        System.out.print("*");
+    }
+        System.out.println();
+        System.out.print("***");
+        for (int i = 0; i < (26 - (msg.length()) / 2); i++) {
+            System.out.print(" ");
+        }
+        System.out.print(msg);
+        for (int i = 0; i < 27 - (msg.length()) / 2; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("***");
+        System.out.println();
+        for (int i = 0; i < 60; i++) {
+            System.out.print("*");
+        }
+    }
+
+
+
 }
